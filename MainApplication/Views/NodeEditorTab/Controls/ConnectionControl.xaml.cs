f45aaa4da1def5415aff149c7b4236be23b5d17b
WhiteCatMonar <torachan.ryouta@gmail.com>
2026-01-11 02:44:32 +0900

    リファクタリング: ViewModelの構造に近くなるようにVIewを調整

---
+++ MainApplication/Views/NodeEditorTab/Controls/ConnectionControl.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace MainApplication.Views.NodeEditorTab.Controls
+{
+    /// <summary>
+    /// 接続線を表示・操作するためのUserControl。
+    /// このクラスはXAML側のUIと結びつくコードビハインドであり、
+    /// 特別なロジックは持たずInitializeComponentの呼び出しのみを担当する。
+    /// </summary>
+    public partial class ConnectionControl : UserControl
+    {
+        public ConnectionControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainApplication/Views/NodeEditorTab/Controls/GridControl.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace MainApplication.Views.NodeEditorTab.Controls
+{
+    /// <summary>
+    /// グリッド線を表示・操作するためのUserControl。
+    /// このクラスはXAML側のUIと結びつくコードビハインドであり、
+    /// 特別なロジックは持たずInitializeComponentの呼び出しのみを担当する。
+    /// </summary>
+    public partial class GridControl : UserControl
+    {
+        public GridControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
